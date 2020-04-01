f8fe19a3a690590d85304a97a409ea398f2baf70
DLR-TK <biemingjie@163.com>
2020-04-01 14:33:13 +0800

    力扣打卡

---
+++ src/main/java/com/tk/algorithm/maximum_nesting_depth_1111/Solution.java
@@ -0,0 +1,20 @@
+package com.tk.algorithm.maximum_nesting_depth_1111;
+
+public class Solution {
+
+    public int[] maxDepthAfterSplit(String seq) {
+
+        int[] res = new int[seq.length()];
+        int depth = 0;
+        for (int i = 0; i <seq.length() ; i++) {
+            if (seq.charAt(i) == '(') {
+                depth++;
+                res[i] = depth % 2;
+            } else {
+                res[i] = depth % 2;
+                depth--;
+            }
+        }
+        return res;
+    }
+}
